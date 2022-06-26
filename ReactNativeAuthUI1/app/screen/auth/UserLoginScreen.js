3d88c795720cf507b067c7428beefe8e726874c8
Muzialliswell <88335098+Muzialliswell@users.noreply.github.com>
2022-06-26 19:27:54 +0500

    dfes
    dffg

--- ReactNativeAuthUI1/app/screen/auth/UserLoginScreen.js
+++ ReactNativeAuthUI1/app/screen/auth/UserLoginScreen.js
@@ -35,7 +35,7 @@ const UserLoginScreen = () => {
       if (res.data.status === "success") {
         await storeToken(res.data.token); // Store Token in Storage
         clearTextInput();
-        navigation.navigate("UserPanelTab");
+        navigation.navigate("SelectedSchool");
       }
       if (res.data.status === "failed") {
         Toast.show({
