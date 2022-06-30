b46b96d10cb35c02ba57cbc75c7e961895fc07d2
saadnadeem2015 <93639683+saadnadeem2015@users.noreply.github.com>
2022-06-30 13:11:06 +0500

    aa

--- ReactNativeAuthUI1/app/screen/UserPanelTab.js
+++ ReactNativeAuthUI1/app/screen/UserPanelTab.js
@@ -17,7 +17,7 @@ const UserPanelTab = () => {
               <Button
                 onPress={()=> navigation.navigate('CreateSchoolScreen') }
                 title="Add School"
-                color="blue"
+                color="#5062BD"
               />
             ),
             headerStyle: {
